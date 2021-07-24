dc9f56361e86f78b49e5af830fce06f6d52b87de
Christian Dibala Bell <ChristianDibalaBell@gmail.com>
2021-07-24 13:34:42 -0400

    [wip] Adds Formik

--- src/Flyout.js
+++ src/Flyout.js
@@ -8,6 +8,34 @@ import {
 import Colors from './Colors';
 import classNames from 'classnames';
 
+// TODO: all this Formik logic may need to live in App, and not Flyout, as that's where all the current state lives ...
+import { Formik, Form, useField } from 'formik';
+
+const MyNumberInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  // debugger;
+  return (
+    // label
+    <div>
+      <input
+        type="number"
+        {...field}
+        {...props}
+        // id="degrees"
+        // name="degrees"
+        // value={degrees}
+        // onChange={event => { setDegrees(event.target.value)}}
+        min={0}
+        max={360}
+      />
+      <label htmlFor={props.id || props.name}>{label}</label>
+      {meta.touched && meta.error ? (
+        <div>{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
 const Flyout = ({
   colors,
   setColors,
@@ -34,60 +62,78 @@ const Flyout = ({
   const onFlyoutButtonClick = () => setIsFlyoutOpen(!isFlyoutOpen);
 
   return (
-    <div className={classnames('flyout', {
-      'flyout--open': isFlyoutOpen,
-      'flyout--closed': !isFlyoutOpen
-    })}>
-      <button
-        className={classNames('flyout__control-button', {
-          'flyout__control-button--open': isFlyoutOpen,
-          'flyout__control-button--closed': !isFlyoutOpen,
-        })}
-        onClick={onFlyoutButtonClick}
-      >
-        {isFlyoutOpen ? 'close' : 'open'}
-      </button>
-      <div className="flyout__content">
-        <div className="card">{gradientString}</div>
-        <div className="options card">
-          <Colors
-            colors={colors}
-            addOrRemoveStopPositionHandler={addOrRemoveStopPositionHandler}
-            stopPercentChangeHandler={stopPercentChangeHandler}
-            deleteColor={deleteColor}
-            inputChangeHandler={inputChangeHandler}
-          />
-          <button
-            onClick={() => {
-              setColors([...colors, {
-                color: randomColorGenerator(),
-                stopPositions: []
-              }])
-            }}
-          >
-            Add a random color
-          </button>
-          <div>
-            <input
-              id="degrees"
-              type="number"
+    <Formik
+      initialValue={{
+        colors,
+        setColors,
+        degrees,
+        setDegrees,
+      }}
+      validationSchema={{}}
+      onSubmit={() => {}}
+    >
+      <Form className={classnames('flyout', {
+        'flyout--open': isFlyoutOpen,
+        'flyout--closed': !isFlyoutOpen
+      })}>
+        <button
+          className={classNames('flyout__control-button', {
+            'flyout__control-button--open': isFlyoutOpen,
+            'flyout__control-button--closed': !isFlyoutOpen,
+          })}
+          onClick={onFlyoutButtonClick}
+          type="button"
+        >
+          {isFlyoutOpen ? 'close' : 'open'}
+        </button>
+        <div className="flyout__content">
+          <div className="card">{gradientString}</div>
+          <div className="options card">
+            <Colors
+              colors={colors}
+              addOrRemoveStopPositionHandler={addOrRemoveStopPositionHandler}
+              stopPercentChangeHandler={stopPercentChangeHandler}
+              deleteColor={deleteColor}
+              inputChangeHandler={inputChangeHandler}
+            />
+            <button
+              onClick={() => {
+                setColors([...colors, {
+                  color: randomColorGenerator(),
+                  stopPositions: []
+                }])
+              }}
+            >
+              Add a random color
+            </button>
+            <MyNumberInput
+              label="degrees"
               name="degrees"
-              value={degrees}
-              onChange={event => { setDegrees(event.target.value)}}
               min={0}
               max={360}
             />
-            <label htmlFor="degrees">degrees</label>
-          </div>
-          <div>
-            <label htmlFor="gradient-type">Gradient Type</label>
-            <select name="gradient-type">
-              <option value="linear">Linear</option>
-            </select>
+            {/* <div>
+              <input
+                id="degrees"
+                type="number"
+                name="degrees"
+                value={degrees}
+                onChange={event => { setDegrees(event.target.value)}}
+                min={0}
+                max={360}
+              />
+              <label htmlFor="degrees">degrees</label>
+            </div> */}
+            <div>
+              <label htmlFor="gradient-type">Gradient Type</label>
+              <select name="gradient-type">
+                <option value="linear">Linear</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
+      </Form>
+    </Formik>
   );
 };
 
